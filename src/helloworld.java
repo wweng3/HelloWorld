59bf34d3ea5368403d9ce865a2ed4c9647f48c36
wweng3 <31376980+wweng3@users.noreply.github.com>
2017-09-30 14:55:46 -0700

    Update helloworld.java

--- src/helloworld.java
+++ src/helloworld.java
@@ -14,7 +14,9 @@ public class helloworld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Hello World");
+		System.out.print("Wilson Weng");
+		System.out.print("CS3337");
 	}
 
 }
